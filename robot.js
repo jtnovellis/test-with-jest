aec0ff2acdcf6582e5961e35d70e8d979c227850
Daniel Moreno <daniel_9127@hotmail.com>
2022-08-31 19:44:43 -0500

    add jest corrections

--- robot.js
+++ robot.js
@@ -1,60 +1,71 @@
 exports.createRobot = (x, y) => {
-  let _x = x;
-  let _y = y;
-  let orientation = 'N';
-  function advance() {
-    switch (orientation) {
-      case 'N': _y += 1;
-      break;
-      case 'S': _y -= 1;
-      break;
-      case 'W': _x -= 1;
-      break;
-      case 'E': _x += 1;
-      break;
+    let _x = x;
+    let _y = y;
+    let orientation = 'N';
+
+    function advance() {
+        switch (orientation) {
+            case 'N':
+                _y += 1;
+                break;
+            case 'S':
+                _y -= 1;
+                break;
+            case 'W':
+                _x -= 1;
+                break;
+            case 'E':
+                _x += 1;
+                break;
+        };
+        if (_x < 0 || _y < 0 || _x > 10 || _y > 10) {
+            return `invalid movement`;
+        };
+        return `advance in the direction ${orientation} and pos x: ${_x} and y: ${_y}`;
     };
-    if (_x < 0 || _y <0 ) {
-      throw new Error('invalid movement');
+
+    function right() {
+        if (orientation === 'N') {
+            orientation = 'E';
+        } else if (orientation === 'S') {
+            orientation = 'W';
+        } else if (orientation === 'W') {
+            orientation = 'N';
+        } else if (orientation === 'E') {
+            orientation = 'S';
+        };
+        return orientation;
     };
-    return `advance in the direction ${orientation} and pos x: ${_x} and y: ${_y}`;
-  };
-  function right() {
-    if(orientation === 'N') {
-      orientation = 'E';
-    } else if(orientation === 'S') {
-      orientation = 'W';
-    } else if(orientation === 'W') {
-      orientation = 'N';
-    } else if(orientation === 'E') {
-      orientation = 'S';
+
+    function left() {
+        if (orientation === 'N') {
+            orientation = 'W';
+        } else if (orientation === 'S') {
+            orientation = 'E';
+        } else if (orientation === 'W') {
+            orientation = 'S';
+        } else if (orientation === 'E') {
+            orientation = 'N';
+        };
+        return orientation;
+    }
+
+    function movement(instructions) {
+        const inst = instructions.split('')
+        let array = []
+        for (let i = 0; i < inst.length; i++) {
+            let element = inst[i]
+            if (element === 'A') {
+                array.push(advance());
+            } else if (element === 'R') {
+                array.push(right());
+            } else if (element === 'L') {
+                array.push(left());
+            } else {
+                return `invalid movement`;
+            };
+        }
+        return `orientarion: "${array[array.length-1]}" and pos x: ${_x}, y: ${_y}`;
     };
-    return orientation;
-  };
-  function left() {
-    if(orientation === 'N') {
-      orientation = 'W';
-    } else if(orientation === 'S') {
-      orientation = 'E';
-    } else if(orientation === 'W') {
-      orientation = 'S';
-    } else if(orientation === 'E') {
-      orientation = 'N';
-    };
-    return orientation;
-  }
-  function movement(instructions) {
-    instructions.split('').map(element => {
-      if (element === 'A') {
-        return advance();
-      } else if (element === 'R') {
-        return right();
-      } else if (element === 'L') {
-        return left();
-      } else {
-        throw new Error('movement invalid');
-      };
-    });
-    return `orientarion: "${orientation}" and pos x: ${_x}, y: ${_y}`;
-  };
-  return { advance, right, left, movement };
-};
+    return { advance, right, left, movement };
+}
